38acabc5cedc3580e76c11dbd336cc71f51454d0
hingham <hannah.c.ingham@gmail.com>
2018-11-22 17:27:06 -0800

    port hotfix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const ejs = require('ejs')
 const bodyParser = require('body-parser');
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 
